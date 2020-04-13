0af401e33246e1262546568328726058cbe24852
Aaron Chafetz <achafetz@usaid.gov>
2020-04-13 10:58:35 -0400

    add achv pct to df

--- 2020-04-13/setup_data.R
+++ 2020-04-13/setup_data.R
@@ -1,8 +1,8 @@
 ## PROJECT:  coRps
 ## AUTHOR:   Chafetz | USAID
-## PURPOSE:  stucture dataset for use in ggplot overview
-## DATE:     2020-03-14
-## UPDATTED: 2020-03-30
+## PURPOSE:  stucture dataset for use in ggplot exercises
+## DATE:     2020-04-13
+## UPDATTED: 
 
 
 # DEPENDENCIES ------------------------------------------------------------
@@ -14,59 +14,58 @@ library(ICPIutilities)
 
   dataset_url <- "https://media.githubusercontent.com/media/ICPI/TrainingDataset/master/Output/MER_Structured_TRAINING_Datasets_PSNU_IM_FY18-20_20200214_v1_1.txt"
   
-  df <- read_msd(dataset_url, save_rds = FALSE)
+  df <- read_msd(dataset_url)
   
 
-# MUNGE HTS --------------------------------------------------------------
 
-  #filter for testing  
-    df_hts <- df %>% 
-      filter(indicator %in% c("HTS_TST", "HTS_TST_POS"),
-             standardizeddisaggregate == "Modality/Age/Sex/Result",
-             fiscal_year == 2020) 
+# LINKAGE -----------------------------------------------------------------
+
+
+  df_linkage <- df %>% 
+    filter(operatingunit == "Saturn",
+           indicator %in% c("HTS_TST_POS", "TX_NEW"),
+           standardizeddisaggregate == "Total Numerator",
+           fiscal_year == 2019) %>% 
+    group_by(operatingunit, primepartner, psnu, indicator, fiscal_year) %>% 
+    summarise_at(vars(cumulative), sum, na.rm = TRUE) %>% 
+    ungroup() %>% 
+    spread(indicator, cumulative) %>% 
+    mutate(linkage = TX_NEW/HTS_TST_POS)
   
-  #aggregate
-    df_hts <- df_hts %>%
-      group_by(fiscal_year, operatingunit, psnu, modality, indicator) %>% 
-      summarise_at(vars(cumulative), sum, na.rm = TRUE) %>% 
-      ungroup()
-    
-  #spread and create indicators
-    df_hts <- df_hts %>% 
-      spread(indicator, cumulative, fill = 0) %>% 
-      filter_at(vars(HTS_TST, HTS_TST_POS), any_vars(. != 0)) %>% 
-      mutate(Positivity = HTS_TST_POS / HTS_TST,
-             is_index = str_detect(modality, "Index"))
-    
+  write_csv(df_linkage, "2020-04-13/FY19_Saturn_linkage.csv", na = "")
+  
 
-# MUNGE TX ----------------------------------------------------------------
 
-  #filter for testing  
-    df_tx <- df %>% 
-      filter(indicator %in% c("TX_NEW", "TX_NET_NEW", "TX_CURR"),
-             standardizeddisaggregate == "Total Numerator",
-             fiscal_year == 2020) 
-    
-  #aggregate
-    df_tx <- df_tx %>%
-      group_by(fiscal_year, operatingunit, fundingagency, primepartner,  indicator) %>% 
-      summarise_at(vars(cumulative), sum, na.rm = TRUE) %>% 
-      ungroup() %>% 
-      filter(cumulative != 0,
-             primepartner != "Dedup")
-    
-  #spread and create indicators
-    df_tx <- df_tx %>% 
-      spread(indicator, cumulative) %>% 
-      filter_at(vars(TX_CURR, TX_NEW), any_vars(!is.na(.)))
-    
-  #additional filters
-    df_tx <- df_tx %>% 
-      filter(TX_NEW < 1000,
-           TX_NET_NEW > -2000)
-    
-# EXPORT ------------------------------------------------------------------
+# TREATMENT TRENDS --------------------------------------------------------
 
-  write_csv(df_hts, "2020-03-30/FY20_MilkyWay_Testing.csv", na = "")  
-  write_csv(df_tx, "2020-03-30/FY20_MilkyWay_NewtoNetNew.csv", na = "")  
+  df_trends <- df %>% 
+    filter(operatingunit == "Jupiter",
+           indicator %in% c("TX_CURR", "TX_NEW", "TX_NET_NEW"),
+           standardizeddisaggregate == "Total Numerator") %>% 
+    group_by(operatingunit,  indicator, fiscal_year) %>% 
+    summarise_at(vars(starts_with("qtr")), sum, na.rm = TRUE) %>% 
+    ungroup() %>% 
+    reshape_msd(clean = TRUE) %>% 
+    arrange(indicator, period)
+  
+  write_csv(df_trends, "2020-04-13/FY18-20_Jupiter_txtrends.csv", na = "")
+  
+  
+
+# TARGET ACHIEVEMENT ------------------------------------------------------
+
+  df_achievement <- df %>% 
+    filter(operatingunit == "Neptune",
+           indicator == "TX_NEW",
+           standardizeddisaggregate == "Age/Sex/HIVStatus",
+           fiscal_year == 2020) %>% 
+    group_by(fiscal_year, operatingunit, primepartner, indicator, sex) %>% 
+    summarise_at(vars(cumulative, targets), sum, na.rm = TRUE) %>% 
+    ungroup() %>% 
+    filter(primepartner != "Dedup") %>% 
+    mutate(achievement = cumulative / targets)
+  
+  write_csv(df_achievement, "2020-04-13/FY20_Neptune_txnew_achievement.csv", na = "")
+  
     
+  
