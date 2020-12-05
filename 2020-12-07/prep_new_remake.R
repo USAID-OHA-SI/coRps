bc1eadef5b3a41b06f4a97ec5e85e29730d14e82
tessam30 <tim.essam@gmail.com>
2020-12-04 20:46:34 -0500

    add in glitr intro

---
+++ 2020-12-07/prep_new_remake.R
@@ -0,0 +1,25 @@
+# Purpose: Show the process of making the PrEP_NEW plot
+# Author: Tim Essam | SIEI
+# Date: 2020-12-07
+# Notes: coRps Session
+
+
+# GLOBALS -----------------------------------------------------------------
+
+  library(tidyverse)
+  library(glitr)
+  library(gisr)
+  library(glamr)
+  
+
+
+  data_path <- "../Zambezi/Data/"
+
+  # To print the colors and their values
+  
+  color_list <- list(denim, old_rose, moody_blue, burnt_sienna, scooter, golden_sand, genoa,
+                     usaid_blue, usaid_red, usaid_black, usaid_lightblue, usaid_medblue, 
+                     usaid_darkgrey, usaid_medgrey, usaid_lightgrey)
+
+  color_list
+  
